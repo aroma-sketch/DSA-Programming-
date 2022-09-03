3f108b60aaba8e888ec8485aeb3751f2b5ea6f34
AROMA GUPTA <aroma.dev@codeoffun.com>
2022-09-03 23:32:18 +0530

    added 2largestelement

---
+++ Array/LargElem2.java
@@ -0,0 +1,45 @@
+package Array;
+import java.util.*;
+
+
+//SECOND LARGEST ELEMENT KIN AN ARRAY
+
+public class LargElem2 {
+    public static void main(String[] args) throws Exception {
+
+    Scanner ob=new Scanner (System.in);
+    int i;
+    
+    //size of array
+    System.out.print("size=");
+     int n =ob.nextInt();
+
+     //elements
+    System.out.print("elem in array:");
+    int a[]= new int[n];
+    for( i=0;i<n;i++)
+    {
+        a[i]=ob.nextInt();
+           
+    }
+    //algo
+
+    int largest=0,second=0;
+     for(i=0;i<n;i++)
+     {
+        if(largest<a[i])
+        {
+             second=largest;
+            largest=a[i];
+
+        }
+         if(second<a[i] && largest!=a[i])
+         {
+            second=a[i];
+         }
+     }
+     System.out.println("second largest:"+second);
+     System.out.println(" largest:"+largest);
+}
+}
+
