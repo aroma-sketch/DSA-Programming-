4df81ec7286d7576eea1d1752295f4ab13606df8
AROMA GUPTA <aroma.dev@codeoffun.com>
2022-11-29 00:07:36 +0530

    print subarrays

--- Array/PrintSubarray.java
+++ Array/PrintSubarray.java
@@ -1,43 +1,41 @@
 package Array;
+
 import java.util.*;
+
 // print all the sub arrays
-public class PrintSubarray 
-{
-  static  void  Sub_arrays(int a[],int n)
-    {
-int i,j,k=0;
-for(i=0;i<n;i++)
-{
-    for(j=i;j<n;j++)
-    {
-        for(k=i;k<=j;k++)
+public class PrintSubarray {
+    static void Sub_arrays(int a[], int n) {
+        int i, j, k = 0;
+        for (i = 0; i < n; i++) 
         {
-            System.out.print(a[k]+" ");
+            for (j = i; j < n; j++)
+             {
+                for (k = i; k <= j; k++)
+                 {
+                    System.out.print(a[k] + " ");
+                }
+                System.out.println();
+            }
         }
-        System.out.println();
-    }
-}
 
     }
-    
+
     public static void main(String[] args) throws Exception {
-        
-    Scanner ob=new Scanner (System.in);
-    int i;
-    
-    //size of array
-    System.out.print("size=");
-     int n =ob.nextInt();
 
-     //elements
-    System.out.print("elem in array:");
-    int a[]= new int[n];
-        for( i=0;i<n;i++)
-        {
-            a[i]=ob.nextInt();
+        Scanner ob = new Scanner(System.in);
+        int i;
+
+        // size of array
+        System.out.print("size=");
+        int n = ob.nextInt();
+
+        // elements
+        System.out.print("elem in array:");
+        int a[] = new int[n];
+        for (i = 0; i < n; i++) {
+            a[i] = ob.nextInt();
         }
-    Sub_arrays(a,n);
-        // System.out.println(sa+" ");
+        Sub_arrays(a, n);
 
     }
 }
